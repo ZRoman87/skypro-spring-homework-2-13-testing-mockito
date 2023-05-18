1c4301f8b814dea5468c73fa6a8857fe8f11766b
ZRoman87 <zrazhevskiy.roman@yandex.ru>
2023-05-18 08:05:34 +0300

    First Commit

---
+++ src/main/java/com/example/skyprospringhomework213testingmockito/SkyproSpringHomework213TestingMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprospringhomework213testingmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHomework213TestingMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHomework213TestingMockitoApplication.class, args);
+	}
+
+}
